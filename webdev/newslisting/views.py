d4a720af7013dd1bdf35fa699b814580f48ab067
Katelyn Eustace <keustace@Katelyns-MacBook-Pro-445.local>
2019-10-11 21:04:13 -0400

    starter code for backend

---
+++ webdev/newslisting/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import 
+
+# Create your views here.
