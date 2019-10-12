f377281f80c30b642165b521aabac1060cf33a38
Ryan Polhemus <ryan@polhemus.org>
2019-10-12 02:01:51 -0400

    Now returns doc_vectors, word_list

--- src/data_analysis/classification.py
+++ src/data_analysis/classification.py
@@ -65,7 +65,7 @@ def keywordify(documents) :
     all_freqs = Counter()
     keywords = [None] * num_docs
 
-    # Clean all docs
+    # Clean and tokenize all docs
     for doc_idx in range(num_docs) :
         sent_tokens = tokenizer.tokenize(documents[doc_idx].strip())
         word_tokens = [word_tokenizer.word_tokenize(sent) for sent in sent_tokens]
@@ -80,6 +80,7 @@ def keywordify(documents) :
     for term in all_freqs :
         idf[term] = math.log(num_docs / len([doc_idx for doc_idx in range(num_docs) if term_freqs[doc_idx][term] > 0]))
 
+    # Calculate tfidfs for each term in each doc
     for doc_idx in range(num_docs) :
         tfidf = {}
         for term in term_freqs[doc_idx] :
@@ -91,4 +92,14 @@ def keywordify(documents) :
             print("Error in except, keywordify 1")
             keywords[doc_idx] = tfidf
 
-    return keywords
+    doc_vecs = [None] * num_docs
+
+    for doc_idx in range(num_docs) :
+        word_dict = { term:0 for term in all_freqs}
+        for term in keywords[doc_idx] :
+            word_dict[term] = term_freqs[doc_idx][term]
+        doc_vecs[doc_idx] = list(word_dict.values())
+
+    word_list = list(all_freqs.keys())
+
+    return doc_vecs, word_list
