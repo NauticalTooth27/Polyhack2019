0f0e390454c7e3b1ea7d7cb6b3433c51f3b439fd
milesizydorczak12 <56458475+milesizydorczak12@users.noreply.github.com>
2019-10-12 10:18:40 -0400

    pizza

--- src/website/js/script.js
+++ src/website/js/script.js
@@ -60,21 +60,12 @@ function popupArticle(url,article){
     printArticleSmart(popupWindow,article);
 }
 
-function scrollingUp(height, width, speed) {
-  var elem = document.getElementById("animate");
-    width = width / 10 * 3;
-    var count = 0;
-  var pos = 0;
-  var id = setInterval(frame, speed);
-  function frame () {
-    if (pos == height) {
-            elem.style.right = (count * 30) % width + 'px';
-            count++;
-      pos = -30;
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-    }
-  }
-}
+
+
+
+
+
+
+
+
 
