9187b9e16e147b14f12908db20f120eb0832229b
jonathanconroy <jonathanconroy14@gmail.com>
2019-10-12 12:14:02 -0400

    SQL integration

--- src/data_analysis/fetch_articles.py
+++ src/data_analysis/fetch_articles.py
@@ -2,19 +2,22 @@ from requests_html import HTMLSession
 # IMPORTANT - use websockets==6.0 (not more recent version)
 import os
 import random
+import numpy as np
 import sqlite3
 from newsapi import NewsApiClient
+from multiprocessing import Pool
 
 # Set constants
 NEWS_API_KEY = os.environ["NEWS_API_KEY"]
-
+NUM_PROC = 2
+NUM_SAMPLES_PER_SOURCE = 2
 
 newsapi = NewsApiClient(api_key=NEWS_API_KEY)
 conn = sqlite3.connect("documents.db")
 c = conn.cursor()
-#c.execute('''CREATE TABLE documents(title text, source text, url text,
-#             author text, description text, publishedAt Date, content text)''')
-#conn.commit()
+c.execute('''CREATE TABLE documents(title text, source text, url text,
+             author text, description text, content text)''')
+conn.commit()
 
 '''
 1. Get list of news from newsapi
@@ -27,7 +30,7 @@ source2content = {
 "the-huffington-post": ".content-list-component",
 "politico": ".story-text__paragraph",
 #"slate": ".slate-paragraph",
-"abc-news": ".article-body",
+"abc-news": "[itemprop=articleBody]",
 "fox-news": ".article-body",
 "the-hill": ".field-item",
 #"westernjournal": ".entry-content",
@@ -53,39 +56,51 @@ def get_content(url, source):
     return content
 
 def articles_about_subject(subject):
-    filtered_articles = list()
+    documents = list()
+    articles = list()
     for source in all_sources:
         top_headlines = newsapi.get_everything(sources = source, q = subject)
 
         if not top_headlines["articles"]:
             print("None found: ", subject, source)
             continue
-        filtered_articles.append(random.choice(top_headlines["articles"]))
 
-    documents = list()
-    for article in filtered_articles:
-        url = article["url"]
-        source = article["source"]["id"]
+        ordered_articles = np.random.permutation(np.array(top_headlines["articles"]))
+        sample_size = min(len(ordered_articles), NUM_SAMPLES_PER_SOURCE)
+        ordered_articles = ordered_articles[:sample_size]
+        print(ordered_articles[0])
+        p = Pool(NUM_PROC)
+        content_list = p.map(save_article, ordered_articles)
+
+def save_article(article):
+    url = article["url"]
+    source = article["source"]["id"]
+    try:
         content = get_content(url, source)
-        documents.append(content)
-
-    return documents
+        if content:
+            save_to_db(article, content)
+    except Exception as e:
+        print("    ERROR: ", url)
+        print("    ",e)
 
 def save_to_db(article, content):
     title = article["title"]
     source = article["source"]["name"]
     url = article["url"]
     author = article["author"]
-    publishedAt = article["Date"]
-    c.execute("INSERT INTO documents VALUES ({}, {}, {}, {}, {}, {}, {})".format(
-        title, source, url, author, publishedAt, content))
+    description = article["description"]
+    c.execute("INSERT INTO documents(title, source, url, author, description, content) VALUES (?, ?, ?, ?, ?, ?)", (title, source, url, author, description, content))
+    conn.commit()
+
+def sample():
+    articles_about_subject("Giuliani")
+    articles_about_subject("impeachment")
+    articles_about_subject("Turkey")
 
 def all_articles():
     filtered_articles = list()
     for source in all_sources:
         top_headlines = newsapi.get_everything(sources = source, pageSize = 100)
-        for top_headlines["articles"]
-        filtered_articles.extend(random.choice(top_headlines["articles"]))
 
     documents = list()
     for article in filtered_articles:
@@ -96,7 +111,6 @@ def all_articles():
 
     return documents
 
-for i in range(len(documents)):
-    with open("../../resources/sample_documents/document{}.txt".format(i), "w+") as file:
-        print(documents[i])
-        file.write(documents[i])
+sample()
+conn.commit()
+conn.close()
